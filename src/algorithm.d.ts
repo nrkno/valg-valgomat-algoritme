324518f41b7592600cd5bce1ec4ac8583fac1367
Stian Veum Møllersen <stian.moellersen@gmail.com>
2025-04-23 12:13:24 +0200

    chore: update typedefs

--- src/algorithm.d.ts
+++ src/algorithm.d.ts
@@ -1,12 +1,13 @@
-type PositionKey = string
+type PositionValue = -2 | -1 | 1 | 2;
 
 type Positions = {
-  [key: PositionKey]: { value: -2 | -1 | 0 | 1 | 2 | null | undefined }
-}
+  [key: string]: { value: PositionValue | null };
+};
 
-export function distance(positionsA: Positions, positionsB: Positions): number
-export function distanceMap<T extends string>(
+type PositionsMap<T extends string> = Record<T, Positions>;
+
+export function proximity(positionsA: Positions, positionsB: Positions): number;
+export function proximityMap<T extends string>(
   positionsA: Positions,
-  positionsMap: { [key in T]: Positions },
-  weights?: { [key: PositionKey]: number }
-): { [key in T]: number }
+  positionsMap: PositionsMap<T>
+): Record<T, number>;
