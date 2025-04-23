0e7efa0741fa298c421e9bda06ae7ea243c912f9
Stian Veum Møllersen <stian.moellersen@gmail.com>
2025-04-23 13:01:22 +0200

    chore: upgrade prettier, add prettier config and run formatting

--- src/domain/positions.js
+++ src/domain/positions.js
@@ -1,28 +1,28 @@
 function toVector(positions) {
-  return Object.keys(positions).map(function(statementId) {
-    return [statementId, parseFloat(positions[statementId].value)];
-  });
+  return Object.keys(positions).map(function (statementId) {
+    return [statementId, parseFloat(positions[statementId].value)]
+  })
 }
 
 function toPositions(vector) {
   return vector.reduce((positions, [id, value]) => {
-    positions[id] = { value };
-    return positions;
-  }, {});
+    positions[id] = { value }
+    return positions
+  }, {})
 }
 
 function removeNotAnswered(vector) {
-  return vector.filter(function([, position]) {
-    return !isNaN(position);
-  });
+  return vector.filter(function ([, position]) {
+    return !isNaN(position)
+  })
 }
 
 function toStatementSet(vector) {
-  return vector.map(([id]) => id);
+  return vector.map(([id]) => id)
 }
 
 function commonStatements(statementSetA, statementSetB) {
-  return statementSetA.filter((id) => statementSetB.includes(id));
+  return statementSetA.filter((id) => statementSetB.includes(id))
 }
 
 module.exports = {
@@ -31,4 +31,4 @@ module.exports = {
   removeNotAnswered,
   toStatementSet,
   commonStatements,
-};
+}
