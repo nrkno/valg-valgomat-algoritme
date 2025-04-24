f1c357a981dd3b7c975f5112d1b533f33f4a2808
Stian Veum Møllersen <stian.moellersen@gmail.com>
2025-04-24 10:23:06 +0200

    chore: add types and typechecking with jsconfig

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,70 +1,76 @@
-import {
-  toVector,
-  removeNotAnswered,
-  toStatementSet,
-  commonStatements
-} from "./domain/positions.js";
+import { commonStatements } from './domain/positions.js'
 
-import { pipe } from "./domain/functions.js";
+/** @type {ValgomatAlgoritme.PositionValue} */
+const MIN_POSITION = -2
+/** @type {ValgomatAlgoritme.PositionValue} */
+const MAX_POSITION = 2
+const MAX_DIST = Math.abs(MAX_POSITION - MIN_POSITION)
 
-const MIN_POSITION = -2;
-const MAX_POSITION = 2;
-const MAX_DIST = Math.abs(MAX_POSITION - MIN_POSITION);
-
+/**
+ * @param {string[]} statements
+ * @param {ValgomatAlgoritme.Positions} positionsA
+ * @param {ValgomatAlgoritme.Positions} positionsB
+ * @returns {[ValgomatAlgoritme.PositionValue, ValgomatAlgoritme.PositionValue][]}
+ */
 function combineBy(statements, positionsA, positionsB) {
-  return statements.map(function(statementId) {
-    let a = positionsA[statementId];
-    let b = positionsB[statementId];
+  return statements.map(function (statementId) {
+    // NOTE: These casts are safe because we always call this with `statements`
+    // being the set of common statements between the two positions.
+    let a = /** @type {{value: ValgomatAlgoritme.PositionValue}} */ (positionsA[statementId])
+    let b = /** @type {{value: ValgomatAlgoritme.PositionValue}} */ (positionsB[statementId])
 
-    return [a.value, b.value];
-  });
+    return [a.value, b.value]
+  })
 }
 
+/**
+ * @param {ValgomatAlgoritme.PositionValue} a
+ * @param {ValgomatAlgoritme.PositionValue} b
+ * @returns
+ */
 function difference(a, b) {
-  return Math.abs(a - b);
+  return Math.abs(a - b)
 }
 
+/**
+ * @param {string[]} statements
+ * @param {ValgomatAlgoritme.Positions} positionsA
+ * @param {ValgomatAlgoritme.Positions} positionsB
+ * @returns {number | null}
+ */
 function distanceGivenStatements(statements, positionsA, positionsB) {
   if (statements.length === 0) {
-    // NOTE:
-    // If there are no statements, the difference would be unknowable. So we
-    // return null in this case. This used to return 0 for historical reasons,
-    // but this reason is no longer valid and we need to distinguish between
-    // furthest apart (0% common) and unknown.
-    return null;
+    // NOTE: If there are no statements, the difference would be unknowable. So
+    // we return null in this case. This used to return 0 for historical
+    // reasons, but this reason is no longer valid and we need to distinguish
+    // between furthest apart (0% common) and unknown.
+    return null
   }
 
   let distance = combineBy(statements, positionsA, positionsB)
     .map(([a, b]) => difference(a, b))
-    .reduce((sum, value) => sum + value);
+    .reduce((sum, value) => sum + value)
 
-  let maxPossibleDistance = statements.length * MAX_DIST;
+  let maxPossibleDistance = statements.length * MAX_DIST
 
-  return (maxPossibleDistance - distance) / maxPossibleDistance;
+  return (maxPossibleDistance - distance) / maxPossibleDistance
 }
 
+/** @type {import('../types.d.ts').proximity} */
 export function proximity(positionsA, positionsB) {
-  let processPositions = pipe(
-    toVector,
-    removeNotAnswered,
-    toStatementSet
-  );
+  let answeredStatements = commonStatements(positionsA, positionsB)
 
-  let statementsA = processPositions(positionsA);
-  let statementsB = processPositions(positionsB);
-
-  let answeredStatements = commonStatements(statementsA, statementsB);
-
-  return distanceGivenStatements(answeredStatements, positionsA, positionsB);
+  return distanceGivenStatements(answeredStatements, positionsA, positionsB)
 }
 
+/** @type {import('../types.d.ts').proximityMap} */
 export function proximityMap(positionsA, positionsMap) {
-  let result = {};
+  let result = /** @type {Record.<string, number|null>} */ ({})
   for (let id in positionsMap) {
-    let positionsB = positionsMap[id];
-    let calculatedDistance = proximity(positionsA, positionsB);
-    result[id] = calculatedDistance;
+    let positionsB = positionsMap[id]
+    let calculatedDistance = proximity(positionsA, positionsB)
+    result[id] = calculatedDistance
   }
 
-  return result;
+  return result
 }
--- src/domain/functions.js
+++
@@ -1,5 +0,0 @@
-export function pipe(...fns) {
-  return function (x) {
-    return fns.reduce((v, f) => f(v), x)
-  }
-}
